af7fce3d17046cb9367f717cab1424bcf7ba6620
QuantumGod2021 <85337078+kflict@users.noreply.github.com>
2021-06-16 21:44:12 -0500

    Create index.js

---
+++ Users/josephkahn/Documents/ORGIN/get-secretmanager-secrets/dist/index.js
@@ -0,0 +1,17 @@
+
+	// html4
+	if (!res && str) {
+		res = /<meta[\s]+?http-equiv=(['"])content-type\1[\s]+?content=(['"])(.+?)\2/i.exec(str);
+		if (!res) {
+			res = /<meta[\s]+?content=(['"])(.+?)\1[\s]+?http-equiv=(['"])content-type\3/i.exec(str);
+			if (res) {
+				res.pop(); // drop last quote
+			}
+		}
+		if (res) {
+			res = /charset=(.*)/i.exec(res.pop());
+				// HTTP fetch step 5.5
+				switch (request.redirect) {
+					case 'error':
+						reject(new FetchError(`uri requested responds with a redirect, redirect mode is set to error: ${request.url}`, 'no-redirect'));
+						finalize();
